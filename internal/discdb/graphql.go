39a1315cee5ca1f42d0fc83eaf3acd14450c4abf
Maddux <37754975+m-macdonald@users.noreply.github.com>
2026-06-26 00:53:27 +0000

    Updated graphql query. (#21)
    TheDiscDb's GraphQL schema changed. Updating to use the new structure,
    resolving errors.

--- internal/discdb/graphql.go
+++ internal/discdb/graphql.go
@@ -2,47 +2,47 @@ package discdb
 
 const getDiscByContentHash = `
 	query GetDiscByContentHash($hash: String) {
-		mediaItems (
-			where: {
-				releases: {some: {discs: {some: {contentHash: {eq: $hash}}}}}
-			}
-		) {
-			nodes {
-				title
-				year
-				slug
-				type
-				releases (
-					where: {discs: {some: {contentHash: {eq: $hash}}}}
-				) {
-					slug
-					locale
-					year
-					title
-					discs (
-						where: {contentHash: {eq: $hash}}
-					) {
-						contentHash
-						index
-						name
-						format
-						slug
-						titles {
-							index
-							duration
-							displaySize
-							sourceFile
-							size
-							segmentMap
-							item {
-								title
-								season
-								episode
-								type
-							}
-						}
-					}
-				}
-			}
+	  mediaItems(
+		where: {
+		  releases: {some: {discs: {some: {disc: {contentHash: {eq: $hash}}}}}}
 		}
-    }`
+	  ) {
+		nodes {
+		  title
+		  year
+		  slug
+		  type
+		  releases(
+			where: {discs: {some: {disc: {contentHash: {eq: $hash}}}}}
+		  ) {
+			slug
+			locale
+			year
+			title
+			discs(
+			  where: {disc: {contentHash: {eq: $hash}}}
+			) {
+			  contentHash
+			  index
+			  name
+			  format
+			  slug
+			  titles {
+				index
+				duration
+				displaySize
+				sourceFile
+				size
+				segmentMap
+				item {
+				  title
+				  season
+				  episode
+				  type
+				}
+			  }
+			}
+		  }
+		}
+	  }
+	}`
